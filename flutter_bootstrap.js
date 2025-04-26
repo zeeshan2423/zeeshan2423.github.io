44a63bcce364882cf1b83d72fcf0c93e3336cb5a
zeeshan2423 <zeeshan2423@gmail.com>
2025-04-27 01:48:14 +0530

    added logo for website

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4164619452"
+    serviceWorkerVersion: "3163761539"
   }
 });
